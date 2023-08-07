cf23f8b55dbdd3d9ec403521b19eaeea58abff25
Nikolai <nikolaiptukhin@gmail.com>
2023-08-07 13:04:11 -0400

    Final

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+function Banner(){
+    return(
+       <div>
+        <h1 calssName="header">"Orbit Report"</h1>
+        <p>Click on the buttons to see the satellites in that orbit type </p>
+        </div>
+    );
+ }
+ 
+ export default Banner;
